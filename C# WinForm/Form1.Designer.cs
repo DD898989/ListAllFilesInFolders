e86c19a33f53e6772da3fb1255640a5cc0050ebd
DD898989 <killuplus300@gmail.com>
2019-03-18 21:22:45 +0800

    Edited

---
+++ C# WinForm/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApplication32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Run = new System.Windows.Forms.Button();
+            this.textBox_ProcessBar = new System.Windows.Forms.TextBox();
+            this.textBox_OutputFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_OutputFileName = new System.Windows.Forms.TextBox();
+            this.button_SelectFolder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox_SearchFolders = new System.Windows.Forms.ListBox();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.button_Remove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Run
+            // 
+            this.button_Run.Location = new System.Drawing.Point(189, 238);
+            this.button_Run.Name = "button_Run";
+            this.button_Run.Size = new System.Drawing.Size(75, 23);
+            this.button_Run.TabIndex = 0;
+            this.button_Run.Text = "Run";
+            this.button_Run.UseVisualStyleBackColor = true;
+            this.button_Run.Click += new System.EventHandler(this.button_Run_Click);
+            // 
+            // textBox_ProcessBar
+            // 
+            this.textBox_ProcessBar.Enabled = false;
+            this.textBox_ProcessBar.Location = new System.Drawing.Point(76, 267);
+            this.textBox_ProcessBar.Name = "textBox_ProcessBar";
+            this.textBox_ProcessBar.Size = new System.Drawing.Size(317, 22);
+            this.textBox_ProcessBar.TabIndex = 1;
+            // 
+            // textBox_OutputFolder
+            // 
+            this.textBox_OutputFolder.Location = new System.Drawing.Point(14, 46);
+            this.textBox_OutputFolder.Name = "textBox_OutputFolder";
+            this.textBox_OutputFolder.Size = new System.Drawing.Size(420, 22);
+            this.textBox_OutputFolder.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output File Name";
+            // 
+            // textBox_OutputFileName
+            // 
+            this.textBox_OutputFileName.Location = new System.Drawing.Point(452, 46);
+            this.textBox_OutputFileName.Name = "textBox_OutputFileName";
+            this.textBox_OutputFileName.Size = new System.Drawing.Size(84, 22);
+            this.textBox_OutputFileName.TabIndex = 5;
+            // 
+            // button_SelectFolder
+            // 
+            this.button_SelectFolder.Location = new System.Drawing.Point(359, 74);
+            this.button_SelectFolder.Name = "button_SelectFolder";
+            this.button_SelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.button_SelectFolder.TabIndex = 6;
+            this.button_SelectFolder.Text = "Select Folder";
+            this.button_SelectFolder.UseVisualStyleBackColor = true;
+            this.button_SelectFolder.Click += new System.EventHandler(this.button_SelectFolder_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(440, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(8, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "\\";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Search Folders";
+            // 
+            // listBox_SearchFolders
+            // 
+            this.listBox_SearchFolders.AllowDrop = true;
+            this.listBox_SearchFolders.FormattingEnabled = true;
+            this.listBox_SearchFolders.HorizontalScrollbar = true;
+            this.listBox_SearchFolders.ItemHeight = 12;
+            this.listBox_SearchFolders.Location = new System.Drawing.Point(14, 114);
+            this.listBox_SearchFolders.Name = "listBox_SearchFolders";
+            this.listBox_SearchFolders.Size = new System.Drawing.Size(456, 100);
+            this.listBox_SearchFolders.TabIndex = 9;
+            this.listBox_SearchFolders.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox_SearchFolders.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // button_Add
+            // 
+            this.button_Add.Location = new System.Drawing.Point(476, 162);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(75, 23);
+            this.button_Add.TabIndex = 10;
+            this.button_Add.Text = "Add";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // button_Remove
+            // 
+            this.button_Remove.Location = new System.Drawing.Point(476, 191);
+            this.button_Remove.Name = "button_Remove";
+            this.button_Remove.Size = new System.Drawing.Size(75, 23);
+            this.button_Remove.TabIndex = 11;
+            this.button_Remove.Text = "Remove";
+            this.button_Remove.UseVisualStyleBackColor = true;
+            this.button_Remove.Click += new System.EventHandler(this.button_Remove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 304);
+            this.Controls.Add(this.button_Remove);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.listBox_SearchFolders);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_SelectFolder);
+            this.Controls.Add(this.textBox_OutputFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_OutputFolder);
+            this.Controls.Add(this.textBox_ProcessBar);
+            this.Controls.Add(this.button_Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Run;
+        private System.Windows.Forms.TextBox textBox_ProcessBar;
+        private System.Windows.Forms.TextBox textBox_OutputFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_OutputFileName;
+        private System.Windows.Forms.Button button_SelectFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox_SearchFolders;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Button button_Remove;
+    }
+}
+
---
+++ C# WinForm/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication32
+{
+    public partial class Form1 : Form
+    {
+        List<string> _listString = new List<string>(); //tested: init List with space needed won't improve efficiency in Add function
+        int _nEvery = 0;
+        int _nFiles;
+        long _nSize;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox_OutputFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            this.textBox_OutputFileName.Text = "AllFile.txt";
+            this.listBox_SearchFolders.Items.Add("C:\\");
+            this.listBox_SearchFolders.Items.Add("D:\\");
+        }
+
+        void ListAdd(string type, string directoryCode, string directoryCode_or_file, string fileCount_or_lastEditTime, string createTime, string size)
+        {
+            _listString.Add(type + "\t" + directoryCode + "\t" + directoryCode_or_file + "\t" + fileCount_or_lastEditTime + "\t" + createTime + "\t" + size);
+        }
+
+        void Run()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            watch.Reset();
+            watch.Start();
+
+            if (File.Exists(Path.Combine(this.textBox_OutputFolder.Text, this.textBox_OutputFileName.Text)))
+                File.Delete(Path.Combine(this.textBox_OutputFolder.Text, this.textBox_OutputFileName.Text));
+
+            ListAdd(
+                "【D=directory】【f=file】",
+                "【directory code】",
+                "【directory code】【file】",
+                "【file count】【last edit time】",
+                "【create time】",
+                "【size】"
+                );
+
+
+            foreach (string path in this.listBox_SearchFolders.Items)
+            {
+                _nFiles = 0;
+                _nSize = 0;
+                DirSearch(path);
+            }
+            writeFile();
+
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds / 1000);
+            this.textBox_ProcessBar.Text = "done";
+        }
+
+        void writeFile()
+        {
+            TextWriter tw = new StreamWriter(Path.Combine(this.textBox_OutputFolder.Text, this.textBox_OutputFileName.Text), true, Encoding.Unicode);
+
+            foreach (string s in _listString)
+                tw.WriteLine(s);
+            tw.Close();
+
+            _listString.Clear();
+        }
+
+        void DirSearch(string sDir)
+        {
+            long ori_size = _nSize;
+            int ori_num = _nFiles;
+            long new_size = 0;
+            int new_num = 0;
+            string[] directories;
+            FileInfo[] files;
+            DirectoryInfo dir;
+            string sDirCode = "";
+
+            try
+            {
+                directories = Directory.GetDirectories(sDir);
+            }
+            catch /*(System.Exception excpt)*/
+            {
+                /*listString.Add("Error1 occured: " + excpt.Message);*/
+                return;
+            }
+
+            if (sDir.Length < 15)
+                sDirCode = sDir.ToString();
+            else
+                sDirCode = sDir.GetHashCode().ToString();
+
+
+            foreach (string d in directories)
+            {
+                DirSearch(d);
+
+                new_size += _nSize;
+                new_num += _nFiles;
+
+                _nSize = ori_size;
+                _nFiles = ori_num;
+            }
+
+            _nSize = new_size;
+            _nFiles = new_num;
+
+            try
+            {
+                dir = new DirectoryInfo(sDir);
+                files = dir.GetFiles();
+            }
+            catch /*(System.Exception excpt)*/
+            {
+                /*listString.Add("Error2 occured: " + excpt.Message);*/
+                return;
+            }
+
+            long sizeMB;
+            bool bPrintDir = false;
+            foreach (FileInfo f in files)
+            {
+                _nSize += f.Length;
+                _nFiles++;
+                sizeMB = f.Length / 1024 / 1024;
+
+                //if (
+                //sizeMB > 10
+                //&&
+                //DateTime.Compare(new DateTime(2019, 2, 18, 0, 0, 0), f.LastWriteTime) < 0
+                //||
+                //f.ToString().ToLower().Contains(".txt")
+                //    )
+                {
+                    bPrintDir = true;
+                    ListAdd(
+                            "f",
+                            sDirCode,
+                            Path.GetFileName(f.ToString())/*FileInfo only contains full path file*/,
+                            f.LastWriteTime.ToString("yyyy/MM/dd HH:mm:ss"),
+                            f.CreationTime.ToString("yyyy/MM/dd HH:mm:ss"),
+                            sizeMB.ToString()
+                        );
+                }
+
+                _nEvery++;
+
+                if (_nEvery % 500 == 0)
+                {
+                    this.textBox_ProcessBar.Text = new string('*', (_nEvery / 500) % 50); //user experience
+                    Application.DoEvents();
+                    if (_nEvery % 50000 == 0)
+                        writeFile();
+                }
+
+            }
+
+            sizeMB = _nSize / 1024 / 1024;
+            //if (
+            //    bPrintDir
+            //    sizeMB > 5000
+            //    )
+            {
+                ListAdd(
+                    "D",
+                    sDir,
+                    sDirCode,
+                    _nFiles.ToString(),
+                    dir.CreationTime.ToString("yyyy/MM/dd HH:mm:ss"),
+                    sizeMB.ToString()
+                    );
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Run_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            Run();
+            this.Enabled = true;
+        }
+
+        private void button_SelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox_OutputFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.listBox_SearchFolders.Items.Add(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string fileName in files)
+            {
+                listBox_SearchFolders.Items.Add(fileName);
+            }
+        }
+
+        private void button_Remove_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedObjectCollection selectedItems = new ListBox.SelectedObjectCollection(this.listBox_SearchFolders);
+            selectedItems = this.listBox_SearchFolders.SelectedItems;
+            int index = listBox_SearchFolders.SelectedIndex;
+            if (this.listBox_SearchFolders.SelectedIndex != -1)
+            {
+                for (int i = selectedItems.Count - 1; i >= 0; i--)
+                    this.listBox_SearchFolders.Items.Remove(selectedItems[i]);
+            }
+
+            try
+            {
+                listBox_SearchFolders.SelectedIndex = index;
+                return;
+            }
+            catch 
+            {
+            }
+
+            try
+            {
+                listBox_SearchFolders.SelectedIndex = index-1;
+                return;
+            }
+            catch
+            {
+            }
+        }
+    }
+}
